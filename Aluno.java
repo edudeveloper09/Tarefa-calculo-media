5368982a7e197c20d57c13a4ce24ad4b6ebde150
edudeveloper09 <157771672+edudeveloper09@users.noreply.github.com>
2024-04-04 17:10:21 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+public class Aluno {
+
+    public static void main(String[] args) {
+
+        int nota1 = 5;
+        int nota2 = 3;
+        int nota3 = 5;
+        int nota4 = 8;
+        int soma1 = nota1 + nota2;
+        int soma2 = nota3 + nota4;
+        int resultado = (soma1 + soma2) /4;
+
+        System.out.println(resultado);
+    }
+}
